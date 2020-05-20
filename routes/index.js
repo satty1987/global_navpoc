1caba359234b8c3f6e1e2ac0c8ec7d5f8240b968
Satnam Singh <satnam.singh156@t-mobile.com>
2020-05-21 00:21:41 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ const config = require('../public/config/microapp.config.json');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { path: 'payment-app',type:'microapp', userInfo: req.userContext.userinfo, config: JSON.stringify(config) });
+  res.render('index', { path: 'payment-app',type:'microapp',  config: JSON.stringify(config) });
 });
 router.get('/payment', function (req, res, next) {
-  res.render('index', { path: 'payment-app',type:'microapp', userInfo: req.userContext.userinfo,config: JSON.stringify(config) });
+  res.render('index', { path: 'payment-app',type:'microapp',config: JSON.stringify(config) });
 });
 router.get('/upgrade', function (req, res, next) {
-  res.render('index', { path: 'react-app',type:'microapp',userInfo: req.userContext.userinfo, config: JSON.stringify(config) });
+  res.render('index', { path: 'react-app',type:'microapp', config: JSON.stringify(config) });
 });
 router.get('/iframe', function (req, res, next) {
-  res.render('index', { path: 'coverage-map',type:'iframe',userInfo: req.userContext.userinfo, config: JSON.stringify(config) });
+  res.render('index', { path: 'coverage-map',type:'iframe', config: JSON.stringify(config) });
 });
 
 module.exports = router;
